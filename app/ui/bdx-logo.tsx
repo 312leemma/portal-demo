43bb67e77f20d7a27b650ae928c786c55dc0ad0f
312leemma <312leemma@gmail.com>
2025-12-01 15:21:13 +1100

    Fix: 重新上传完整项目代码

--- app/ui/bdx-logo.tsx
+++ app/ui/bdx-logo.tsx
@@ -1,13 +1,15 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 
-export default function AcmeLogo() {
+export default function BDXLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <Image src="/logo.png" width={260} height={60} alt = "Biosignals DX Logo" />
+      {/* <p className="text-[44px]">BiosignalsDX</p> */}
     </div>
   );
 }
+
